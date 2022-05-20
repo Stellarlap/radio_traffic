c9a5df14b6e83496a83b7fe4fe92911f88643c69
Stellarlap <100299314+Stellarlap@users.noreply.github.com>
2022-05-19 21:28:08 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,9 @@
 input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(1)
+    radio.sendValue("Fisher", 1)
+    radio.sendValue("Dynes", 2)
 })
-basic.showLeds(`
-    . . # . .
-    . . # . .
-    # # # # #
-    . . # . .
-    . . # . .
-    `)
+input.onButtonPressed(Button.B, function () {
+    radio.sendValue("Fisher", 2)
+    radio.sendValue("Dynes", 3)
+})
 radio.setGroup(58)
